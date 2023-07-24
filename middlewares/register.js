4d4f740d3dbe9ed6ad03d84a8a62078760bff75d
Randa Kourifa <randa.kourifa@gmail.com>
2023-07-24 21:11:15 +0100

    try13

--- middlewares/register.js
+++ middlewares/register.js
@@ -121,3 +121,63 @@ export const registerUser = async (req, res, next) => {
     });
   }
 };
+// Remplacez 'votre_clé_secrète' par la clé secrète que vous souhaitez utiliser pour signer les tokens JWT
+const JWT_SECRET =
+  "randaquiessayedereparersontokenpourlapremierfois123456789try2";
+
+export const ADDUser = async (req, res, next) => {
+  // Get user input
+  const { first_name, last_name, email, password } = req.body;
+
+  // Validate user input
+  if (!(email && password && first_name && last_name)) {
+    return res.status(400).json({
+      message: "All input is required",
+    });
+  }
+
+  try {
+    // check if user already exists
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+      return res.status(409).json({
+        message: "User Already Exists. Please Login",
+      });
+    }
+
+    // Encrypt user password
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const newUser = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: newUser._id, email: newUser.email },
+      JWT_SECRET, // Utilisez la clé secrète définie dans la constante JWT_SECRET
+      {
+        expiresIn: "2h",
+      }
+    );
+
+    // Save the token to the user document in MongoDB
+    newUser.token = token;
+    await newUser.save();
+
+    // Add the user to the request object for later use
+    req.user = newUser;
+
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Error creating user",
+      error: err.message,
+    });
+  }
+};
